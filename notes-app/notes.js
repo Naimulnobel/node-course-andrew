a503f202d3b569107fe1a3220c1c7f43f272bd85
naimulnobel <nobelnaimulislam@gmail.com>
2022-01-07 23:42:40 +0600

    remove function add

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,53 @@
+const fs = require('fs')
+
 const getNotes = function () {
     return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = function (title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added!')
+    } else {
+        console.log('Note title taken!')
+    }
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return notes.pop(note.title === title) 
+    })
+    console.log(notes)
+}
+
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+}
