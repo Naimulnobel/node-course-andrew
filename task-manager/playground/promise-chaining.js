8c636d8782f85f51c8b153ce14b1e109a648fe34
naimulnobel <nobelnaimulislam@gmail.com>
2022-01-09 01:25:23 -0800

    promise chaining

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,12 +1,23 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
-User.findByIdAndUpdate("61da5e9ce1feed06e7fb71c2", {age:27}).then(
-    (user) => {
-        console.log(user)
-        return User.countDocuments({age:26})
-    }
-).then((result)=> {
+const Task = require('../src/models/task')
+// User.findByIdAndUpdate("61da5e9ce1feed06e7fb71c2", {age:27}).then(
+//     (user) => {
+//         console.log(user)
+//         return User.countDocuments({age:26})
+//     }
+// ).then((result)=> {
+//     console.log(result)
+// }).catch((err) => {
+//     console.log(err)
+// })
+Task.deleteMany({completed: false}).then(function(){
+    console.log("Data deleted"); // Success
+    return Task.countDocuments({completed:true})
+}).then((result)=>{
     console.log(result)
-}).catch((err) => {
-    console.log(err)
-})
+}
+
+).catch(function(error){
+    console.log(error); // Failure
+});
